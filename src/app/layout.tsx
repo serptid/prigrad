c1bb5a9e22143d4b577353a9cb6cd1590d13569d
Сергей Прокопенко <psi200523@gmail.com>
2025-04-27 19:36:33 +1000

    sp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
+import SPWMini from 'spwmini/client';
+import { useEffect } from "react";
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const spm = new SPWMini('8346d39a-bc0e-48b8-9f4b-85c37a32e55a')
+    spm.initialize()
+  }, []);
+
+
   return (
     <html lang="en">
       <body>
